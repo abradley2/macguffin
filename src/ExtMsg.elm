3401b95eb2985432c40a72681332f22145280f27
Tony <antbradley91@gmail.com>
2020-05-13 11:09:31 -0400

    add test for redirect

--- src/ExtMsg.elm
+++ src/ExtMsg.elm
@@ -1,4 +1,4 @@
-module ExtMsg exposing (ExtMsg(..), Log, Token(..))
+module ExtMsg exposing (ExtMsg(..), Log, Token(..), hasRedirect)
 
 
 type Token =
@@ -16,3 +16,19 @@ type ExtMsg
     | ReplaceUrl String
     | PushUrl String
     | Batch (List ExtMsg)
+
+
+hasRedirect : ExtMsg -> Bool
+hasRedirect extMsg =
+    case extMsg of
+        PushUrl _ -> True
+        Batch msgList ->
+            List.foldl
+                (\cur acc ->
+                    if acc then acc
+                    else hasRedirect cur
+                )
+                False
+                msgList
+        _ ->
+            False
--- tests/DashboardTest.elm
+++ tests/DashboardTest.elm
@@ -1,16 +1,17 @@
 module DashboardTest exposing (..)
 
-import ComponentResult exposing (applyExternalMsg, mapError, mapModel, mapMsg, resolve)
+import ComponentResult exposing (applyExternalMsg, escape, mapError, mapModel, mapMsg, resolve)
 import Expect exposing (Expectation)
-import ExtMsg exposing (Token(..))
+import ExtMsg exposing (Token(..), hasRedirect)
 import Flags exposing (Flags)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Html.Attributes as A
+import Http
 import Page.Dashboard as DashboardPage
 import RemoteData exposing (RemoteData(..))
 import Test exposing (..)
 import Test.Html.Query as Query
 import Test.Html.Selector as Selector
-import Html.Attributes as A
 
 
 flags : Flags
@@ -27,7 +28,13 @@ getModel =
         >> Tuple.first
 
 
-token = Just (Token "token")
+getExtMsg =
+    escape >> Result.toMaybe >> Maybe.andThen (\( _, _, extMsg ) -> extMsg)
+
+
+token =
+    Just (Token "token")
+
 
 suite : Test
 suite =
@@ -78,7 +85,25 @@ suite =
                     |> getModel
                     |> DashboardPage.view token flags
                     |> Query.fromHtml
-                    |> Query.findAll [ Selector.attribute <| A.attribute "data-test" "protocols-modal"]
+                    |> Query.findAll [ Selector.attribute <| A.attribute "data-test" "protocols-modal" ]
                     |> Query.count (Expect.equal 0)
+            )
+        , test "Should redirect to login if agent profile 401's"
+            (\_ ->
+                DashboardPage.init token flags
+                    |> getModel
+                    |> DashboardPage.update
+                        flags
+                        (DashboardPage.FetchedUserProfile <| Result.Err (Http.BadStatus 401))
+                    |> getExtMsg
+                    |> Maybe.map
+                        (\extMsg ->
+                            if hasRedirect extMsg then
+                                Expect.pass
+
+                            else
+                                Expect.fail "Extmsg didn't contain a redirect"
+                        )
+                    |> Maybe.withDefault (Expect.fail "No extMsg")
             )
         ]
